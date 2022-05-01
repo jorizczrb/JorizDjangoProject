3c5d3a3876867e0ad4cf41c272d50d8c5ad89e19
jorizczrb <104712744+jorizczrb@users.noreply.github.com>
2022-05-01 20:29:54 +0800

    Update settings.py

--- JoProject/JoProject/settings.py
+++ JoProject/JoProject/settings.py
@@ -84,17 +84,9 @@ WSGI_APPLICATION = 'JoProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'django_JoProject',
-'USER': 'root',
-'PASSWORD': "",
-'HOST': "",
-'PORT': "",
-'OPTIONS': {
-'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-}
-}
+‘default’: {
+‘ENGINE’: ‘django.db.backends.sqlite3’,
+‘NAME’: os.path.join(BASE_DIR, ‘db.sqlite3’),
 }
 
 
